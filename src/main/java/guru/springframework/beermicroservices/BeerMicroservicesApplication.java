6c602bad63dca5f630af3dd9d7aece332bdce96f
Juan Lara <max_lara3005@hotmail.com>
2022-09-21 09:28:23 -0500

    First commit of project

---
+++ src/main/java/guru/springframework/beermicroservices/BeerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.beermicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/beermicroservices/BeerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.beermicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
